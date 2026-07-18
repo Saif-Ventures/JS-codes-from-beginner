70f77306f35327ea370813c8960a969ebe70ee9b
Saif-Ventures <saifneedsometime@gmail.com>
2026-07-19 01:21:47 +0600

    finished array

---
+++ Arrays-10/m.js
@@ -0,0 +1,23 @@
+// let marks = [66,88,90,43,100]; 
+// console.log (marks);
+// console.log(marks.length); //property
+
+// console.log (marks[0]); // that is indices of arrays. 
+
+let heroes = ["ironman","superman","antman" ];
+
+// to print all the value, we use loops 
+
+//  FOR LOOPS
+
+// for(let i = 0; i < heroes.length ; i++){
+//     console.log(heroes[i]);
+// }
+
+// also we can use for in loop and for of loops. 
+
+// FOR OF LOOPS
+
+// for (let hero of heroes){
+//     console.log(hero.toUpperCase());
+// }
