a9cbd6020cfbcc3d1f39e7cb9ff3c20200161dce
Saif-Ventures <saifneedsometime@gmail.com>
2026-07-27 02:24:01 +0600

    finished DOM part-01

---
+++ DOM-11/script.js
@@ -0,0 +1,17 @@
+// let items = document.querySelector("p");// we can access html with this query selector.
+// console.log(items);
+
+let div = document.querySelector("div");
+console.log(div);
+
+console.log(div.innerText);
+console.log(div.innerHTML);
+
+//if i need to change any text in html . we can  simply do this 
+
+// let para = document.querySelector("h1");
+// console.log(para);
+
+// console.log(para.innerText = "New line");
+
+
