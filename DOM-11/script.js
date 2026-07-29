bcc2f18b7296f327197f9c78e0c170e86ed98f26
Saif-Ventures <saifneedsometime@gmail.com>
2026-07-29 16:02:58 +0600

    finished DOM

--- DOM-11/script.js
+++ DOM-11/script.js
@@ -1,11 +1,11 @@
 // let items = document.querySelector("p");// we can access html with this query selector.
 // console.log(items);
 
-let div = document.querySelector("div");
-console.log(div);
+// let div = document.querySelector("div");
+// console.log(div);
 
-console.log(div.innerText);
-console.log(div.innerHTML);
+// console.log(div.innerText);
+// console.log(div.innerHTML);
 
 //if i need to change any text in html . we can  simply do this 
 
@@ -13,5 +13,38 @@ console.log(div.innerHTML);
 // console.log(para);
 
 // console.log(para.innerText = "New line");
+
+// we can access our attributes with js 
+
+// let div = document.querySelector("div");
+// console.log(div);
+// let d = div.getAttribute("id");
+// console.log(d);
+
+// // if need to add something then
+
+// console.log(div.setAttribute("id","New"));
+
+// we cam change style of any node with this 
+
+// let para = document.querySelector("p");
+// para.style.backgroundColor ="green";
+
+// to insert anything 
+
+// let newPara = document.createElement("p");
+// newPara.innerText = "hi saif";
+// console.log(newPara);
+
+// let div = document.querySelector("div");
+// div.append(newPara); // adds at the end
+// div.prepend(newPara); // adds at the start
+// div.before(newPara); // adds before element (outside)
+// div.after(newPara); // adds after the element (outside)
+
+
+
+
+
 
 
