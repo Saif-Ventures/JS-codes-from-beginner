6f9eac1556d859adc86a1e6cff0a6ce1d2649450
Saif-Ventures <saifneedsometime@gmail.com>
2026-07-30 01:10:59 +0600

    finished Events

---
+++ Events-12/script.js
@@ -0,0 +1,36 @@
+// let btn = document.querySelector("button");
+// btn.onclick = ()=>{
+//     console.log (" button is clicked");
+//     let a = 12;
+//     a++;
+//     console.log(a);
+// }
+
+// let div = document.querySelector(".box");
+// div.onmouseover = ()=>{
+//     console.log("You are inside the div");
+// }
+
+// EVENT LISTENER 
+
+    // let btn = document.querySelector("button");
+    // btn.addEventListener("click",(e)=>{
+    //     console.log("the button was clcked - handler 1");
+    //     console.log(e);
+    //     console.log(e.type);
+    //     console.log(e.target);
+    //     console.log(e.clientX , e.clientY);
+    // })
+
+    // btn.addEventListener("click",()=>{
+    //     console.log("the button was clcked - handler 2");
+    // })
+
+    //  const handler3 = ()=>{
+    //     console.log("the button was clcked - handler 3")
+    //     ;
+    // };
+
+    // // to remove one handler it should be in a variable.
+
+    // btn.removeEventListener("click", handler3);
